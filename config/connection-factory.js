1fc127594ce1bac58e9f9b1ded9a1aef5611ecb4
upgradeagencia <156694505+upgradeagencia@users.noreply.github.com>
2024-01-15 13:13:55 -0300

    Update connection-factory.js

--- config/connection-factory.js
+++ config/connection-factory.js
@@ -2,11 +2,11 @@ const mysql = require('mysql2');
 
 module.exports = function(){
     return mysql.createConnection({
-       host: "127.0.0.1",
+       host: "viaduct.proxy.rlwy.net",
         user: "root",
-        password: "@ITB123456",
-        database: "upgrade",
-        port: "3306"
+        password: "baBbe23-De1ghF-c--266aa51fB43FDF",
+        database: "railway",
+        port: "11746"
     });
 
 };
